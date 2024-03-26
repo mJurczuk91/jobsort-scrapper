5385c751b344dd69b46f97bddc8dd4fe659c5202
Michal Jurczuk <michal.jurczuk91@gmail.com>
2024-03-26 21:21:44 +0100

    fix: added delay to cookiebutton and popup check

--- src/scraping/pracujpl/skipPopup.js
+++ src/scraping/pracujpl/skipPopup.js
@@ -1,6 +1,8 @@
+import { delay } from "../../lib.js";
+
 export default async function skipPopup(page) {
     const popupCloseSelector = '.popup_p1c6glb0';
-
+    await delay(1000);
     const popupButton = !!await page.$(popupCloseSelector);
 
     if(popupButton){
