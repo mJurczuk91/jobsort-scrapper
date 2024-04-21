5ba2e3edba88c647d5d90107c28cbfa3e5d9cc7c
mJurczuk91 <mjurczuk91@gmail.com>
2024-04-21 21:29:14 +0200

    fix: skipCookies typo

--- src/scraping/util/skipCookies.js
+++ src/scraping/util/skipCookies.js
@@ -1,7 +1,6 @@
 import { delay } from "../../lib.js";
 
-export default async function skipCookies(page) {
-    const cookieButtonSelector = '[data-test="button-submitCookie"]';
+export default async function skipCookies(page, cookieButtonSelector) {
     await delay(1000);
     const cookieButton = !!await page.$(cookieButtonSelector);
 
