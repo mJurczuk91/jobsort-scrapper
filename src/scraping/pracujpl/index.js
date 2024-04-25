41bd211ad990447d60b051cb4ef8ef036b351a6a
mJurczuk91 <mjurczuk91@gmail.com>
2024-04-25 14:44:44 +0200

    fix: if pracujpl finds no offer links, it logs an error and returns an empty array

--- src/scraping/pracujpl/index.js
+++ src/scraping/pracujpl/index.js
@@ -1,10 +1,8 @@
 import parseOfferLink from "./parseOfferLink.js";
 import puppeteer from "puppeteer";
-import { checkIfLinkIsInDatabase, delay } from "../../lib.js";
 import { logError } from "../../lib.js";
 import searchResultsParser from "./searchResultsParser.js";
 import scrapeOfferLinks from "../util/scrapeOfferLinks.js";
-import skipPopup from "./skipPopup.js";
 import skipCookies from "../util/skipCookies.js";
 import parseOfferArray from "../util/parseOfferArray.js";
 
@@ -37,8 +35,8 @@ export default async function scrapePracujpl() {
 
     const offerLinks = await scrapeOfferLinks(page, searchLinks, searchResultsParser);
     if (offerLinks.length === 0) {
-        console.log('no offers found in parsepracujpl');
-        return;
+        logError('no offers found in parsepracujpl');
+        return [];
     }
 
     const parsedOffers = await parseOfferArray(offerLinks, 'pracuj.pl', techLookedFor, page, parseOfferLink);
