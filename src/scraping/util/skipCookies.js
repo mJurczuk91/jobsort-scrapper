f4b64153e9393fc2fbf93b26d01a1f0585eb67d4
mJurczuk91 <mjurczuk91@gmail.com>
2024-04-25 13:27:33 +0200

    fix: skipCookies await removed and typo fixed

--- src/scraping/util/skipCookies.js
+++ src/scraping/util/skipCookies.js
@@ -1,12 +1,9 @@
-import { delay } from "../../lib.js";
-
 export default async function skipCookies(page, cookieButtonSelector) {
-    await delay(1000);
     const cookieButton = !!await page.$(cookieButtonSelector);
 
     if (cookieButton) {
         await page.click(cookieButtonSelector, {
-            waitUntil: 'domcontentloaded'
+            waitUntil: 'domcontentoaded',
         });
     }
 }
